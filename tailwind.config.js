a2cd75b53f192456f9744c85775e011861d3b2cd
Muhammed Yaseen <Muhammed.Yaseen@gardinia.ae>
2026-02-02 15:07:05 +0400

    Add Tailwind v4 config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
